7ac12c72a193750997eb8420671384062b164d48
AndrewCGray <graycandrew@gmail.com>
2022-02-23 08:38:28 -0600

    work

---
+++ OFFICIAL PROJECT/Assets/DataPOCManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataPOCManager : MonoBehaviour
+{
+    public static DataPOCManager Instance;
+    public int num;
+    public Text text;
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    public void numChange(int val)
+    {
+        this.num += val;
+    }
+
+    void Update()
+    {
+        text.text = this.num.ToString();
+    }
+}
