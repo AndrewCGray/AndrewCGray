3bce79ff8062bd7ea488611caa7c0e1bc47146a7
AndrewCGray <graycandrew@gmail.com>
2022-02-28 09:24:53 -0600

    Lots of work

---
+++ OFFICIAL PROJECT/Assets/HouseTriggerScrypt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HouseTriggerScrypt : MonoBehaviour
+{
+    public int triggerScene; //The Scene we want to transition to
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(triggerScene);
+    }
+}
