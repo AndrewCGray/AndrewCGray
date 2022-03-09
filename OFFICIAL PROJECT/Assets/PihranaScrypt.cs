a6dbbfc260083fa0f360310556c1d3f6a111a427
AndrewCGray <graycandrew@gmail.com>
2022-03-09 08:39:14 -0600

    w

---
+++ OFFICIAL PROJECT/Assets/PihranaScrypt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PihranaScrypt : MonoBehaviour
+{
+
+    public float speed;
+    bool dead;
+    bool flipx = true;
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 1) * speed;
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().flipX = flipx;
+    }
+
+    public void FaceCorrectDirection(float x, float y)
+    {
+        if(x < t.position.x)
+        {
+            flipx = true;
+        }
+        else
+        {
+            flipx = false;
+        }
+    }
+}
